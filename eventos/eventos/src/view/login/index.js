873d48b45a8f265750857d162118a56df900c5c7
VittorioBarella <vittorio.br@hotmail.com>
2020-09-16 14:59:57 -0300

    Criando a View de Login da Aplicação.

---
+++ eventos/eventos/src/view/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <h1>Login</h1>
+    )
+}
+
+export default Login;
+// Quando alguém invocar este arquivo index.js, o certo é devolver a função de login.
